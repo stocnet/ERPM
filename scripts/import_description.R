bfdf8b0df9c40c73689d70208a6fbaace02f8c80
Marion Hoffman <hoffman@gremaq2.univ-tlse1.fr>
2022-04-20 14:26:13 +0200

    clean exact calculations, with creating imports for bell and combn functions

--- scripts/import_description.R
+++ scripts/import_description.R
@@ -1,4 +1,8 @@
 install.packages("usethis")
 library(usethis)
-usethis::use_package("igraph")
-usethis::use_package("RColorBrewer")
+
+usethis::use_package("numbers",type = "Depends")
+usethis::use_package("utils",type = "Imports")
+
+#usethis::use_package("gmp",type = "Imports")
+#usethis::use_package("mclust",type = "Imports")
