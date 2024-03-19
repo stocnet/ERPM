9affad0f6f142943d0747b96eb720571db9c2cf2
Marion Hoffman <marion.hoffman.31@gmail.com>
2024-03-19 17:22:08 +0100

    Corrected bugs related to the CUP documentation and some tests related to the number_ties function

--- tests/testthat/test_number_ties.R
+++ tests/testthat/test_number_ties.R
@@ -31,22 +31,6 @@ test_that("same ties isolates", {
   expect_equal(number_ties(p1,at3,sta), 0)
 
 })
-# PB pour at3
-
-partition <- c(1,2,2,3,3,4)
-
-dyadic_attribute = at3
-dist<- as.matrix(dist(partition, diag = T, upper = T))
-net_partition <- dist
-net_partition[dist == 0] <- 1
-net_partition[dist != 0] <- 0
-net_partition[is.na(dist)] <- 0
-diag(net_partition) <- 0
-
-numties <- sum(dyadic_attribute * net_partition) / 2
-avgroupnumties <- sum(dyadic_attribute * net_partition) / (2*max(partition, na.rm = T))
-indnumties <- sum( rowSums(dyadic_attribute * net_partition) > 0 )
-avgind <- indnumties/length(partition)
 
 
 test_that("same ties isolates", {
@@ -60,10 +44,10 @@ test_that("same ties isolates", {
   sta <- 'sum_perind'
   expect_equal(number_ties(p1,at1,sta), 0)
   expect_equal(number_ties(p1,at2,sta), 0)
-  expect_equal(number_ties(p1,at3,sta), 4)
+  expect_equal(number_ties(p1,at3,sta), 0)
 
 })
-#SAME PB
+
 test_that("numer ties isolates", {
   p1 <- 1:6
 
@@ -75,7 +59,7 @@ test_that("numer ties isolates", {
   sta <- 'avg_perind'
   expect_equal(number_ties(p1,at1,sta), 0)
   expect_equal(number_ties(p1,at2,sta), 0)
-  expect_equal(number_ties(p1,at3,sta), 4/6)
+  expect_equal(number_ties(p1,at3,sta), 0)
 
 })
 
@@ -88,8 +72,8 @@ test_that("numebr ties  same group", {
   at1 <- diag(1,6)
   at2 <- diag(0,6)
   a <- rep(0,12)
-  at3 <- matrix(c(0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,a),6,6, byrow = T)
-
+  at3 <- matrix(c(0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,a,1,0,0,0,0,0),6,6, byrow = T)
+  
   sta <- 'sum_pergroup'
   expect_equal(number_ties(p1,at1,sta), 0)
   expect_equal(number_ties(p1,at2,sta), 0)
@@ -155,8 +139,8 @@ test_that("numebr ties RANDOM", {
   at1 <- diag(1,6)
   at2 <- diag(0,6)
   a <- rep(0,12)
-  at3 <- matrix(c(0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,a),6,6, byrow = T)
-
+  at3 <- matrix(c(0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,a,1,0,0,0,0,0),6,6, byrow = T)
+  
   sta <- 'sum_pergroup'
   expect_equal(number_ties(p1,at1,sta), 0)
   expect_equal(number_ties(p1,at2,sta), 0)
@@ -179,9 +163,8 @@ test_that("numebr ties RANDOM", {
 
 })
 
-# 2 au lieu de 4 , definition de sum_in , sum indiv , par group ou dans toute la partition?
 
-test_that("numebr ties RANDOM", {
+test_that("number ties RANDOM", {
   p1 <- c(1,2,2,3,3,4)
 
   at1 <- diag(1,6)
