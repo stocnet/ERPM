069ad2d901d6fd3f3d12ea0495d4b0e0ad6d7ab9
nikeis <nico1.keiser@gmail.com>
2024-04-22 15:59:40 +0200

    add outcomeObjects

---
+++ R/outcomeObjects.R
@@ -0,0 +1,14 @@
+#' Exemplary outcome objects for the ERPM Package
+#'
+#' @name outcomeObjects
+#' @description
+#' These are exemplary outcome objects for the ERPM package and can be used in 
+#' order not to run all precedent functions and thus save time. The following 
+#' products are provided:
+
+
+#' @rdname outcomeObjects
+#' @usage NULL
+#' @format `estimation`
+#' An results object created by the function [estimate_ERPM()].
+"estimation"
